3e7f724c269db989c5c920e0faf80a45f1b68471
Lucas Ezequiel Gonçalves França <127765876+lucasezequielfranca@users.noreply.github.com>
2025-12-23 14:58:55 -0300

    game starting made

--- flappy_player.gd
+++ flappy_player.gd
@@ -1,16 +1,26 @@
 extends CharacterBody2D
+class_name Player
+
+@onready var level: Level = $".."
 
 @export var jumpSpeed : int = 300;
+
+var alive : bool = false;
 
 func get_input():
 	if (Input.is_action_just_pressed("jump")):
 		velocity.y = -jumpSpeed;
 
 func _physics_process(delta: float) -> void:
-	velocity += get_gravity() * delta;
-	get_input();
-	move_and_slide();
-	if (velocity.y < 0):
-		rotation_degrees = -35;
-	else:
-		rotation_degrees = 35;
+	if (alive):
+		velocity += get_gravity() * delta;
+		get_input();
+		move_and_slide();
+		if (velocity.y < 0):
+			rotation_degrees = -35;
+		else:
+			rotation_degrees = 35;
+	elif (!alive and !level.game_started):
+		if (Input.is_action_just_pressed("jump")):
+			alive = true;
+			level.game_started = true;
--- pipe.gd
+++ pipe.gd
@@ -1,11 +1,13 @@
 extends AnimatableBody2D
+@onready var level: Level = $".."
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_deferred("global_position", Vector2(global_position.x, randi_range(80, 500)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position.x = position.x - 5;
+	if (level.game_started):
+		position.x = position.x - 5;
